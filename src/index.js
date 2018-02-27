e2c9e6281434ac373948313e7a82c5e3cd9956cb
haronchick <haron.www@mail.ru>
2018-02-27 03:32:50 +0300

    Solution task zeros | add file .gitignore with str .idea

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function getZerosCount(number) {
   // your implementation
+  let check = 0;
+  while (number >= 5) {
+      number = Math.floor(number/5);
+      check = check + number;
+  }
+  return check;
 }
